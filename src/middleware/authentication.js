e30b8ce89e416260fa41ef5f76db6f2897a77ed9
VarelaCristianFacundo <cvarelagarcia@gmail.com>
2024-02-16 18:49:46 -0300

    new token validation for false data

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -1,4 +1,5 @@
 const AUTH_TOKEN = process.env.AUTH_TOKEN;
+const SECOND_TOKEN = process.env.SECOND_TOKEN;
 const logger = require('../config/logger');
 
 module.exports = (req, res, next) => {
@@ -6,9 +7,10 @@ module.exports = (req, res, next) => {
     console.log('Received x-auth-token:', authToken);
     logger.info(`Received x-auth-token: ${authToken}`);
 
-    if (!authToken || authToken !== AUTH_TOKEN) {
+    if (!authToken || (authToken !== AUTH_TOKEN && authToken !== SECOND_TOKEN)) {
         logger.warn('Unauthorized access attempt');
         return res.status(401).json({ error: 'Unauthorized' });
     }
+        
     next();
 };
