611543f763cb8a693c5d18ec609c226a8bcc9ea9
VarelaCristianFacundo <cvarelagarcia@gmail.com>
2024-02-16 14:03:34 -0300

    loggs added in search controller and auth

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -1,10 +1,13 @@
 const AUTH_TOKEN = process.env.AUTH_TOKEN;
+const logger = require('../config/logger');
 
 module.exports = (req, res, next) => {
     const authToken = req.header('x-auth-token');
     console.log('Received x-auth-token:', authToken);
+    logger.info(`Received x-auth-token: ${authToken}`);
 
     if (!authToken || authToken !== AUTH_TOKEN) {
+        logger.warn('Unauthorized access attempt');
         return res.status(401).json({ error: 'Unauthorized' });
     }
     next();
