03331f1c481c99ff9d6a4c1c9f0e7dc022330bfb
VarelaCristianFacundo <cvarelagarcia@gmail.com>
2024-02-16 17:24:34 -0300

    run script fixed

--- src/app.js
+++ src/app.js
@@ -19,7 +19,6 @@ app.use('/search', require('./routes/search'));
 app.use('/items', require('./routes/items'));
 
 app.listen(PORT, () => {
-    logger.info(`Server listening on port ${PORT} in ${NODE_ENV} environment`);
-    console.log(`Server listening on port ${PORT} in ${NODE_ENV} environment`);
-
+    logger.info(`Server listening on port ${PORT}`);
+    console.log(`Server listening on port ${PORT}`);
 });
