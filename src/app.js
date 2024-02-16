351e3de7a25483b3e2cd80d5c3bb6a92f92648b8
VarelaCristianFacundo <cvarelagarcia@gmail.com>
2024-02-16 13:59:38 -0300

    loggs added in item controller

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const swaggerSpec = require('./swaggerConfig');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const dotenv = require("dotenv").config();
+const logger = require('./config/logger');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -18,5 +19,6 @@ app.use('/search', require('./routes/search'));
 app.use('/items', require('./routes/items'));
 
 app.listen(PORT, () => {
+    logger.info(`Server listening on port ${PORT}`);
     console.log(`Server listening on port ${PORT}`);
 });
--- src/config/logger.js
+++ src/config/logger.js
@@ -1,10 +1,21 @@
 const winston = require('winston');
+const fs = require('fs');
+const path = require('path');
 
+const logDirectory = path.join(__dirname, '..', 'logs');
+
+// Creo la carpeta logs si no existe
+if (!fs.existsSync(logDirectory)) {
+  fs.mkdirSync(logDirectory);
+}
+
+// Configuración de logger
 const logger = winston.createLogger({
-  level: 'info', // Nivel de log
+  level: 'info',
   format: winston.format.json(),
   transports: [
     new winston.transports.Console(),
+    new winston.transports.File({ filename: path.join(logDirectory, 'app.log') }),
   ],
 });
 
